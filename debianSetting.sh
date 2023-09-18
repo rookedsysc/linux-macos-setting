89f92620d707ec1e739e63fe6d42ca49a4502ae8
rookedsysc <rookedsysc36@gmail.com>
2023-09-18 10:39:52 -0900

    Update debianSetting

--- debianSetting.sh
+++ debianSetting.sh
@@ -38,11 +38,9 @@ git clone https://github.com/rookedsysc/Linux_MacOS_Setting
 cd ./Linux_MacOS_Setting
 export PATH="$PATH:/usr/bin"
 export PATH="$PATH:/bin"
-cp .zshrc ~/.zshrc
 cp .vimrc ~/.vimrc
 # if dont install zsh-autosuggetions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-source ~/.zshrc
 
 # VIM Setting
 # update Aug 16 21:00 / cuz ubuntu error
@@ -81,3 +79,12 @@ echo "[!] Zsh Setting"
 echo $SHELL
 echo "[!] Power Line Setting"
 which powerline
+
+# ZSH Theme 
+git clone https://github.com/spaceship-prompt/spaceship-prompt "$ZSH_CUSTOM/themes/spaceship"
+ln -s "$ZSH_CUSTOM/themes/spaceship/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+git clone https://github.com/sindresorhus/pure "$ZSH_CUSTOM/themes/pure"
+ln -s "$ZSH_CUSTOM/themes/pure/pure.zsh-theme" "$ZSH_CUSTOM/themes/pure.zsh-theme"
+
+cp .zshrc ~/.zshrc
+source ~/.zshrc
