7d8606f415707df30fd3e93fed025cc346437fcb
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2023-09-20 02:55:12 +0900

    Update and rename debian-setting.sh to root-setting.sh

--- root-setting.sh
+++ root-setting.sh
@@ -27,7 +27,9 @@ apt-get -y install ruby
 
 # ZSH Setting
 chsh -s /bin/zsh
-nohup sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting && git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &
+nohup sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &
+apt-get install -y zsh-syntax-highlighting
+apt-get install -y zsh-autosuggestions
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf 
 ~/.fzf/install
