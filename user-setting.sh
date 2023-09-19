295ef3717307f8af7532d626eae8702091a61fb2
rookedsysc <rookedsysc36@gmail.com>
2023-09-18 15:01:54 -0900

    zsh plugin install fix

--- user-setting.sh
+++ user-setting.sh
@@ -22,9 +22,8 @@ sudo systemctl start ssh
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-# zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+sudo apt-get install -y zsh-syntax-highlighting
+sudo apt-get install -y zsh-autosuggestions
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 git clone https://github.com/rookedsysc/Linux_MacOS_Setting 
