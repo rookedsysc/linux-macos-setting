38d285089fa67c3be4655e19da17bcd0fdcb6170
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2023-09-20 02:31:23 +0900

    Update debian-setting.sh

--- debian-setting.sh
+++ debian-setting.sh
@@ -27,7 +27,7 @@ apt-get -y install ruby
 
 # ZSH Setting
 chsh -s /bin/zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" && exit
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # zsh-autosuggestions
