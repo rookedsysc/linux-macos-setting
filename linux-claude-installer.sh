7070c89619ebdff5e43850fcf1c63d10538c7878
rookedsysc <rookedsysc36@gmail.com>
2025-08-06 18:55:51 +0900

    feat: linux claude code installer 추가

---
+++ linux-claude-installer.sh
@@ -0,0 +1,10 @@
+npm install -g @anthropic-ai/claude-code
+
+# install uv - dependency of superclaude
+curl -Ls https://astral.sh/uv/install.sh | sh
+
+# install superclaude 
+git clone https://github.com/SuperClaude-Org/SuperClaude_Framework.git
+cd SuperClaude_Framework
+uv sync
+
