2713db8baff395ffd1d7b5aec63a17b6f7ae3e3e
rookedsysc <rookedsysc36@gmail.com>
2025-08-14 02:22:21 +0900

    feat: lazygit, lazyvim install 추가

--- user-setting.sh
+++ user-setting.sh
@@ -47,6 +47,17 @@ mv 10-powerline-symbols.conf /etc/fonts/conf.d
 # Zellij
 cargo install --locked zellij
 
+# LazyVim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+# LazyGit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+
 # Setting Check
 echo ""
 echo "[*][*][*][*][*][*][*][*]"
