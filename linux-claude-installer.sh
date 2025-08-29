55d21b9b7fa61019ecb700b3d7e3014fe11b1130
rookedsysc <rookedsysc36@gmail.com>
2025-08-29 17:28:48 +0900

    delete: super claude

--- linux-claude-installer.sh
+++ linux-claude-installer.sh
@@ -1,15 +1,1 @@
 npm install -g @anthropic-ai/claude-code
-
-# install uv - dependency of superclaude
-curl -Ls https://astral.sh/uv/install.sh | sh
-
-# install superclaude 
-git clone https://github.com/SuperClaude-Org/SuperClaude_Framework.git
-cd SuperClaude_Framework
-uv venv
-source .venv/bin/activate
-uv pip install SuperClaude
-uvx pip install SuperClaude
-python3 -m SuperClaude install
-
-
